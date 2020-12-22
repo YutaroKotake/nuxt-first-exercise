e3f4d55430a5ece859f1ef6173bbe26fbd5cd56a
YutaroKotake <kotake@texia.co.jp>
2020-12-22 10:03:00 +0900

    コメント追記

--- pages/index.vue
+++ pages/index.vue
@@ -2,6 +2,7 @@
   <div class="container">
     <!--{{ users[0].id }} , {{ users[0].name }}-->
     <ul>
+      <!-- json配列の「id」、「name」、「companyname」を表示 -->
       <li v-for="user in users" :key="user.id">
         {{user.id}} , {{user.name}}, {{user.company.name}}
       </li>
