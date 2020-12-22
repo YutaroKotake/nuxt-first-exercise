75e934c4effa8627f6f0d190e14de4f6782f0f37
YutaroKotake <kotake@texia.co.jp>
2020-12-22 09:15:09 +0900

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="container">
+    <!--{{ users[0].id }} , {{ users[0].name }}-->
+    <ul>
+      <li v-for="user in users" :key="user.id">
+        {{user.id}} , {{user.name}}, {{user.company.name}}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+const axios = require('axios')
+let url = 'https://jsonplaceholder.typicode.com/users'
+
+export default {
+    asyncData({}){
+        return axios.get(url)
+        .then((res) => {
+            return{users: res.data}
+        })
+    }
+}
+</script>
